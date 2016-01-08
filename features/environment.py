d13a563e764f6e6f4bea75ddcbb8cc11be1f35ed
Kaushal <kaushal.rupani@opexsoftware.com>
2016-01-08 16:39:08 +0530

    [TestNowPython-6]- Adding xpi and code to create UPA hars :sunglasses:

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 browser = os.getenv("BROWSER", "firefox")
 
+
 def before_all(context):
     print("**************** " + browser + " **************")
     if browser == "chrome":
@@ -14,7 +15,17 @@ def before_all(context):
     elif browser == "ie":
         print("A")
     else:
-        context.driver=webdriver.Firefox()
+        profile = webdriver.FirefoxProfile()
+        profile.add_extension(extension="data/har/firebug-2.0.13.xpi")
+        profile.add_extension(extension="data/har/netExport-0.8.xpi")
+        profile.set_preference("extensions.firebug.currentVersion", "2.0.13")
+        profile.set_preference("extensions.firebug.allPagesActivation","on")
+        profile.set_preference("extensions.firebug.defaultPanelName","net")
+        profile.set_preference("extensions.firebug.net.enableSites","true")
+        profile.set_preference("extensions.firebug.netexport.alwaysEnableAutoExport","true")
+        profile.set_preference("extensions.firebug.netexport.defaultLogDir",os.path.abspath("reports/har"))
+
+        context.driver=webdriver.Firefox(firefox_profile=profile)
         context.driver.maximize_window()
         context.driver.implicitly_wait(30)
         context.driver.set_page_load_timeout(120)
