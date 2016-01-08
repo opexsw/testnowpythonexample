842a79430c4290472a629a920f5fed526ea91d10
Kaushal <kaushal.rupani@opexsoftware.com>
2016-01-08 19:13:16 +0530

    [TestNowPython-7]- Clearing browser cookies after each run :cookie:

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,7 @@ import os
 from selenium import webdriver
 
 browser = os.getenv("BROWSER", "firefox")
+har = os.getenv("IS_UPA", "false")
 
 
 def before_all(context):
@@ -15,22 +16,27 @@ def before_all(context):
     elif browser == "ie":
         print("A")
     else:
-        profile = webdriver.FirefoxProfile()
-        profile.add_extension(extension="data/har/firebug-2.0.13.xpi")
-        profile.add_extension(extension="data/har/netExport-0.8.xpi")
-        profile.set_preference("extensions.firebug.currentVersion", "2.0.13")
-        profile.set_preference("extensions.firebug.allPagesActivation","on")
-        profile.set_preference("extensions.firebug.defaultPanelName","net")
-        profile.set_preference("extensions.firebug.net.enableSites","true")
-        profile.set_preference("extensions.firebug.netexport.alwaysEnableAutoExport","true")
-        profile.set_preference("extensions.firebug.netexport.defaultLogDir",os.path.abspath("reports/har"))
+        if har == "true":
+            profile = webdriver.FirefoxProfile()
+            profile.add_extension(extension="data/har/firebug-2.0.13.xpi")
+            profile.add_extension(extension="data/har/netExport-0.8.xpi")
+            profile.set_preference("extensions.firebug.currentVersion", "2.0.13")
+            profile.set_preference("extensions.firebug.allPagesActivation","on")
+            profile.set_preference("extensions.firebug.defaultPanelName","net")
+            profile.set_preference("extensions.firebug.net.enableSites","true")
+            profile.set_preference("extensions.firebug.netexport.alwaysEnableAutoExport","true")
+            profile.set_preference("extensions.firebug.netexport.defaultLogDir",os.path.abspath("reports/har"))
+            context.driver=webdriver.Firefox(firefox_profile=profile)
+        else:
+            context.driver=webdriver.Firefox()
 
-        context.driver=webdriver.Firefox(firefox_profile=profile)
         context.driver.maximize_window()
         context.driver.implicitly_wait(30)
         context.driver.set_page_load_timeout(120)
         context.driver.set_script_timeout(30)
 
+def before_scenario(context,scenario):
+    context.driver.delete_all_cookies()
 
 def after_all(context):
     context.driver.quit()
